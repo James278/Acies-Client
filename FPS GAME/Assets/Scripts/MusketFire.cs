a22e3d6168acce287a6fd50ece3f5b1413bb4d4b
James <jamesperry27@hotmail.co.uk>
2020-03-03 21:00:55 +0000

    Created two scripts. One instantiates smoke animation when clicked. Other destroys smoke animation when it's finished.

---
+++ FPS GAME/Assets/Scripts/MusketFire.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusketFire : MonoBehaviour
+{
+
+    [SerializeField] GameObject smokePrefab;
+
+    [SerializeField] int timeBetweenShots;
+
+    [SerializeField] bool canShoot;
+
+    private void Start()
+    {
+        canShoot = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (canShoot)
+            {
+                GameObject smoke = Instantiate(smokePrefab, transform.position, Quaternion.identity) as GameObject;
+                smoke.transform.parent = gameObject.transform;
+                canShoot = false;
+
+                StartCoroutine(WaitToShoot());
+            }
+        }
+        
+    }
+
+    IEnumerator WaitToShoot()
+    {
+        yield return new WaitForSeconds(timeBetweenShots);
+        canShoot = true;
+    }
+
+}
---
+++ FPS GAME/Assets/Scripts/SmokeLifetime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeLifetime : MonoBehaviour
+{
+
+    float lifeTimeCounter;
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifeTimeCounter += Time.deltaTime;
+        if (lifeTimeCounter >= 10f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
