b8ea17461d7c1fe26ef4be9857ad2d6dce99540c
James <jamesperry27@hotmail.co.uk>
2020-03-19 17:15:51 +0000

    Worked on Musket: Added recoil and aim animations. Made a two scripts: MusketFire and Aim. Firing instantiates smoke and casts a ray

---
+++ FPS GAME/Assets/Scripts/Aim.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+
+    Animator anim;
+
+    Camera FPSCamera;
+
+    [SerializeField] float zoomedInValue;
+
+    float defaultZoomValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        FPSCamera = GetComponentInParent<Camera>();
+
+        defaultZoomValue = FPSCamera.fieldOfView;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            anim.SetBool("Aim", true);
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            anim.SetBool("Aim", false);
+            FPSCamera.fieldOfView = defaultZoomValue;
+        }
+    }
+
+    public void CameraZoom()
+    {
+        FPSCamera.fieldOfView = zoomedInValue;
+    }
+
+}
--- FPS GAME/Assets/Scripts/MusketFire.cs
+++ FPS GAME/Assets/Scripts/MusketFire.cs
@@ -7,37 +7,75 @@ public class MusketFire : MonoBehaviour
 
     [SerializeField] GameObject smokePrefab;
 
-    [SerializeField] int timeBetweenShots;
+    [SerializeField] int timeBetweenShots; // Still to be decided
+
+    [SerializeField] float rayCastRange; // Still to be decided
 
     [SerializeField] bool canShoot;
+
+    [SerializeField] float smokeOffest; // Set to 1
+                    
+    [SerializeField] Vector3 smokeRotOffset; // Set Y to 90
+
+    Animator anim;
+    Camera FPCam;
+    Transform musketChildTransform;
 
     private void Start()
     {
         canShoot = true;
+
+        anim = GetComponent<Animator>();
+        FPCam = gameObject.GetComponentInParent<Camera>();
+        musketChildTransform = transform.Find("Musket"); 
     }
 
     // Update is called once per frame
     void Update()
     {
-
         if (Input.GetMouseButtonDown(0))
         {
             if (canShoot)
             {
-                GameObject smoke = Instantiate(smokePrefab, transform.position, Quaternion.identity) as GameObject;
-                smoke.transform.parent = gameObject.transform;
-                canShoot = false;
+                ActivateSmoke();
+                Recoil();
+                ProcessRayCast();
 
+                canShoot = false;
                 StartCoroutine(WaitToShoot());
             }
         }
         
     }
 
+    private void ActivateSmoke()
+    {
+        GameObject smoke = Instantiate(smokePrefab, transform.position + transform.right * smokeOffest, 
+                                       transform.rotation * Quaternion.Euler(smokeRotOffset)) as GameObject;
+        smoke.transform.parent = gameObject.transform;
+    }
+
+    private void Recoil()
+    {
+        anim.SetTrigger("Shoot");
+    }
+
     IEnumerator WaitToShoot()
     {
         yield return new WaitForSeconds(timeBetweenShots);
         canShoot = true;
+    }
+
+    private void ProcessRayCast()
+    {
+        RaycastHit target;
+        Ray ray = new Ray(musketChildTransform.position, FPCam.transform.forward);
+
+        if (Physics.Raycast(ray, out target, rayCastRange))
+        {
+            print("Ray hit");
+            Debug.DrawRay(musketChildTransform.position, FPCam.transform.forward * target.distance, Color.red);
+        }
     }
 
 }
