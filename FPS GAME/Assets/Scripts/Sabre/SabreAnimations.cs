2ff5ae2719fe43f95d70029ae340437a3d821fa4
James <jamesperry27@hotmail.co.uk>
2020-03-24 14:54:51 +0000

    Made Sabre animations for attacking and blocking. Created two scripts: SabreAnimations and SabreCollisions

---
+++ FPS GAME/Assets/Scripts/Sabre/SabreAnimations.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SabreAnimations : MonoBehaviour
+{
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            anim.SetBool("Ready", true);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            anim.SetBool("Ready", false);
+            anim.SetTrigger("Attack");
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            anim.SetBool("Block", true);
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            anim.SetBool("Block", false);
+        }
+    }
+
+}
---
+++ FPS GAME/Assets/Scripts/Sabre/SabreCollisions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SabreCollisions : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Sabre"))
+        {
+            print("Sabre hit");
+        }
+    }
+}
